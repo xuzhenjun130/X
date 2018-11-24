c702e4f6594db17532a8f94de5e0a9926bbcfb04
sherman <sherman@qq.com>
2018-11-24 22:59:29 +0800

    update CliTool.php

--- CliTool.php
+++ CliTool.php
@@ -15,6 +15,7 @@ class CliTool
     /**
      * 创建模型文件
      * @param $tableName
+     * @throws \Exception
      */
     public function createModel($tableName){
         $tableInfo = X::app()->db->getFields($tableName,false);
@@ -52,24 +53,40 @@ class CliTool
                 $attributes[$v['Field']] = $v['Field'];
             }
         }
-        $attributes = var_export($attributes,true);
+        $attributes = $this->var_export($attributes,true);
         $className = ucfirst($tableName);
         $file = X::app()->basePath.'/models/'.$className.'.php';
         $content = <<<EOF
 <?php
-namespace app\\models;
+namespace models;
 /**
  * Class $className
  $property
  */
- class $className extends X\\Model
+ class $className extends \\X\\Model
  {
-    public function attributes(){
+    public function attributes()
+    {
         return $attributes;
     }
  }
 EOF;
         file_put_contents($file,$content);
         echo 'file create:'.$file;
+    }
+
+    /***
+     * var_export 变量带有方括号，缩进4个空格
+     * @param $expression
+     * @param bool $return
+     * @return mixed|null|string|string[]
+     */
+    private function var_export($expression, $return=FALSE) {
+        $export = var_export($expression, TRUE);
+        $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+        $array = preg_split("/\r\n|\n|\r/", $export);
+        $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+        $export = join(PHP_EOL, array_filter(["["] + $array));
+        if ((bool)$return) return $export; else echo $export;
     }
 }
